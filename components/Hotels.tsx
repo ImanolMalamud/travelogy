fd5b0da14a8f17984df1b8bef37fcce84c70bec3
ImanolMalamud <imanolm93@gmail.com>
2024-10-04 17:01:50 -0300

    removed unused variable

--- components/Hotels.tsx
+++ components/Hotels.tsx
@@ -65,7 +65,7 @@ const Hotels = () => {
     <div className="pt-[5rem] bg-gray-200 pb-[4rem]">
       <h1 className="heading">Best Hotels</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] items-center w-[80%] mx-auto mt-[4rem]">
-        {HOTELS_INFO.map((hotel, i) => (
+        {HOTELS_INFO.map((hotel) => (
           <div
             // data-aos="fade-right"
             // data-aos={`${
